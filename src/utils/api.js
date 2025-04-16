b4463e8b682236b029fed66cb4cf15dba0ecb8f6
Diego Tsukayama <diegohidenori.ca@hotmail.com>
2025-04-16 08:42:39 -0400

    URL fix

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     // baseURL: "http://localhost:5000",
-    baseUrl: process.env.REACT_APP_API_BASE_URL,
+    baseURL: process.env.REACT_APP_API_BASE_URL,
     withCredentials: true, // if needed for cookies, optional
 });
 
